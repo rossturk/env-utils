af16f09445aab5b7306e54005edecece077f69e0
Ross Turk <ross@rossturk.com>
2024-11-23 01:23:30 -0500

    replace function for spinner
    Signed-off-by: Ross Turk <ross@rossturk.com>

--- env-utils.sh
+++ env-utils.sh
@@ -1,17 +1,27 @@
 _gum=${_gum:-@gum@}
 _gum=${_gum//@/}
 
-fxspin() {
-	local title=$1
-	shift
-	local command=("$@")
-
-	# TODO: check to see if we are non-interactive
-	$_gum spin --spinner dot --title "$title" -- "$command"
-}
-
 fxbox() {
 	local args=("$@")
 	$_gum style --border double --margin "1 2" --padding "1 4" "${args[@]}"
+}
+
+fxspin() {
+	local title=$1
+	shift
+
+	 if [[ "$FLOX_ENVS_TESTING" == "1" ]]; then
+		bash -c "$1"
+	else
+		echo
+		gum spin \
+		  --show-error \
+		  --spinner line \
+		  --spinner.foreground="#cccccc" \
+		  --title ">>> $title ..." \
+		  --title.foreground="#cccccc" \
+		    -- bash -c "$@"
+		echo -en "\033[2A\033[K"
+	fi
 }
 
